77d76c83ec4ff7820d50284b59395820feedc1af
hem95 <109483690+hem95@users.noreply.github.com>
2023-10-27 03:12:29 +0530

    Update streamlit_app.py
    imported pandas, read the csv file and displaying it on Streamlit using Streamlit.dataframe command

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,4 @@ streamlit.header("  🥣 🥗 🐔 🥑🍞 Make your Own Breakfast")
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
