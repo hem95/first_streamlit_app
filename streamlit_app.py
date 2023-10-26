d9178cb01f15b125495116def6ba45077c41aef9
hem95 <109483690+hem95@users.noreply.github.com>
2023-10-27 03:11:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,6 @@ streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('  🥣 Hard-Boiled Free-Range Egg')
 streamlit.text("  🥑 Avacado Toast")
 streamlit.header("  🥣 🥗 🐔 🥑🍞 Make your Own Breakfast")
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
