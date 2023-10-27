5016f09147cb650abac8a912087359fa7cd0d24d
hem95 <109483690+hem95@users.noreply.github.com>
2023-10-28 00:17:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 fruit_choice = streamlit.text_input('What fruit would you like to add?')
 streamlit.write("The user entered ", fruit_choice)
-streamlit.text(fruityvice_response)
+
 #create the repeatable block of code called function
 def get_fruityvice_data(this_fruit_choice):
    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
