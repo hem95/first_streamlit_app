cf192b9e19337d2da842834244e4ab46a9232bfc
hem95 <109483690+hem95@users.noreply.github.com>
2023-10-27 23:41:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ streamlit.text(fruityvice_response)
 
 
 #create the repeatable block of code called function
-def get_fruityvice_data(this_fruit_choice)
+def get_fruityvice_data(this_fruit_choice):
    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
    return fruityvice_normalized
