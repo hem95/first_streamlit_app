f4443d8e31e86b60769618e1bc40ae61d7258385
hem95 <109483690+hem95@users.noreply.github.com>
2023-10-27 23:40:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,6 @@ streamlit.text('  🥣 Hard-Boiled Free-Range Egg')
 streamlit.text("  🥑 Avacado Toast")
 streamlit.header("  🥣 🥗 🐔 🥑🍞 Make your Own Breakfast")
 
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -29,29 +28,29 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.write('The user entered ', fruit_choice)
 
-
 streamlit.text(fruityvice_response)
 
 
+#create the repeatable block of code called function
+def get_fruityvice_data(this_fruit_choice)
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   return fruityvice_normalized
+except URLError as e:
+  streamlit.error()
 
-#new section to display fuityvice header
+#new section to display fuityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("please select a fruit or get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
-
-except URLError as e:
-  streamlit.error()
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+   
 
 
-
-
-                                  
 
 streamlit.stop()
 
